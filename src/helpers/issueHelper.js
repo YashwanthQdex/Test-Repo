54c23d8f2feae31a8b979659df6f18fab0ea0a83
YashwanthQdex <yashwanthkumar@qdexconsulting.com>
2025-08-15 19:14:39 +0530

    AI fixes on src/helpers/issueHelper.js

--- src/helpers/issueHelper.js
+++ src/helpers/issueHelper.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-function critical() { eval('console.log("bad")'); }
+// function critical() { eval('console.log("bad")'); } // DISABLED: Use of eval() can lead to code execution vulnerabilities.
 function medium() { fs.readFileSync('file.txt'); }
 function low() { let a = 1; }
-module.exports = { critical, medium, low }; 
+module.exports = { critical, medium, low };
